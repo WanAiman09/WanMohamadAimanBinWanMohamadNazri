e5764f3cec2114a662a0926b2a45de9ae0468999
WanAiman09 <93270995+WanAiman09@users.noreply.github.com>
2022-01-03 18:41:55 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+const email = document.getElementByEmail('email')
+const name = document.getElementByEmail('password')
+const form = document.getElementByEmail('form')
+
+if (password.value.length <=5){
+  messages.push(Password must contain atleast 6 characters )
+}
